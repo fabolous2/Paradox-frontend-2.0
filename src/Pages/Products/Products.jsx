40bb8c614d1cc712f3a60caa1853b49f56388944
yuapX <shesterovmm@gmail.com>
2024-09-27 17:25:52 +0300

    imports fix

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useMemo} from 'react';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import Card from "../../Components/Card/Card";
 import {Dropdown} from '@mui/base/Dropdown';
